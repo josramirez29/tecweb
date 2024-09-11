6642c8145ed0236222d2fec5a1bc48961d550e55
josramirez29 <144729636+josramirez29@users.noreply.github.com>
2024-09-10 22:14:06 -0600

    Ejercicio 2

--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -43,5 +43,47 @@
         <li>myvar: Inválida porque falta el símbolo de $</li>
         <li>$house*5: Inválida porque contiene un carácter especial (*)</li>
     </ul>
+
+    <h2>Ejercicio 2</h2>
+    <p>Proporcionar los valores de $a, $b, $c como sigue:</p>
+    <p>$a = “ManejadorSQL”; $b = 'MySQL’; $c = &$a;</p>
+    <h3>Bloque 1: Asignaciones Iniciales</h3>
+    <h4>a-Asignaciones Iniciales</h4>
+    <?php
+    // Asignaciones iniciales
+    $a = "ManejadorSQL";
+    $b = 'MySQL';
+    $c = &$a;
+
+    // a) Mostrar contenido de cada variable
+    echo '<p>Valores iniciales:</p>';
+    echo '<ul>';
+    echo "<li>\$a = $a</li>";
+    echo "<li>\$b = $b</li>";
+    echo "<li>\$c = $c</li>";
+    echo '</ul>';
+    ?>
+    <h3>Bloque 2: Asignaciones Nuevas</h3>
+    <h4>b-Nuevas Asignaciones y Mostrar contenido</h4>
+    <?php
+    // b) Nuevas asignaciones
+    $a = "PHP server";
+    $b = &$a;
+
+    // c) Mostrar contenido de cada variable
+    echo '<p>Valores después de las nuevas asignaciones:</p>';
+    echo '<ul>';
+    echo "<li>\$a = $a</li>";
+    echo "<li>\$b = $b</li>";
+    echo "<li>\$c = $c</li>"; // $c sigue siendo una referencia a $a
+    echo '</ul>';
+
+    // d) Explicación de lo que ocurrió en el segundo bloque de asignaciones
+    echo '<h4>d-Explicación:</h4>';
+    echo '<p>En el segundo bloque de asignaciones, cuando le asignamos un nuevo valor a <strong>$a</strong>, ese cambio afectó también a <strong>$c</strong>, ya que <strong>$c</strong> es una referencia a <strong>$a</strong>, por lo que ambos contienen el mismo valor, al asignar <strong>$b = &$a</strong>, <strong>$b</strong> también se convierte en una referencia a <strong>$a</strong>, por lo que cualquier cambio en <strong>$a</strong> afectará a <strong>$b</strong> y a <strong>$c</strong>.</p>';
+    // Liberar variables
+    unset($a, $b, $c);
+    ?>
+    
 </body>
 </html>
