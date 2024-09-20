b86028c6849163c2b0c50e1e4b2ef15490cbb9d1
josramirez29 <144729636+josramirez29@users.noreply.github.com>
2024-09-19 19:11:25 -0600

    Ejercicio 1

---
+++ practicas/p06/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Práctica 4</title>
+</head>
+<body>
+    <h1>Práctica 6-Uso de funciones y variables GET y POST en PHP</h1>
+    <h2>Ejercicio 1</h2>
+    <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
+    <?php
+        if(isset($_GET['numero']))
+        {
+            $num = $_GET['numero'];
+            if ($num%5==0 && $num%7==0)
+            {
+                echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
+            }
+            else
+            {
+                echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
+            }
+        }
+    ?>
+
+</body>
+</html>
