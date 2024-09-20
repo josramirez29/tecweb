f6b8b3a6f88c04f640536c37b67eab0f5e7b859d
josramirez29 <144729636+josramirez29@users.noreply.github.com>
2024-09-19 19:38:07 -0600

    Ejercicio 2

--- practicas/p06/index.php
+++ practicas/p06/index.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Práctica 4</title>
+    <title>Práctica 6</title>
 </head>
 <body>
-    <h1>Práctica 6-Uso de funciones y variables GET y POST en PHP</h1>
+    <h1>Práctica 6 - Uso de funciones y variables GET y POST en PHP</h1>
     <h2>Ejercicio 1</h2>
     <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
     <?php
@@ -15,13 +15,54 @@
             $num = $_GET['numero'];
             if ($num%5==0 && $num%7==0)
             {
-                echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
+                echo '<h3>R= El número '.$num.' SI es múltiplo de 5 y 7.</h3>';
             }
             else
             {
                 echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
             }
         }
+    ?>
+
+<h2>Ejercicio 2</h2>
+    <p>Escribir programa para la generación repetitiva de 3 números aleatorios hasta obtener una
+    secuencia compuesta por: impar, par, impar</p>
+    <?php
+        function aleatorio(){
+            return rand(1,100);
+        }
+        function par($numero){
+            return $numero % 2 == 0;
+        }
+        function impar($numero){
+            return $numero%2 != 0;
+        }
+        $iteraciones=0;
+        $matriz= array();
+
+        while(true){
+            $num1 = aleatorio();
+            $num2 = aleatorio();
+            $num3 = aleatorio();
+            $iteraciones++;
+            $matriz[]= array($num1,$num2,$num3);
+            if (impar($num1) && par($num2) && impar($num3)){
+                break;
+            }
+        }
+        $filas = count($matriz);
+        $numeros = $filas * 3;
+        echo "La matriz generada es: <br>";
+        foreach ($matriz as $fila){
+            foreach($fila as $dato){
+            echo $dato. ', ';
+        }
+        echo "<br>";
+    }
+        echo "<br>";
+        echo "Número de iteraciones: $iteraciones <br>";
+        echo "Cantidad de números generados: $numeros";
+    $matriz = array();
     ?>
 
 </body>
--- practicas/p06/src/funciones.php
+++ practicas/p06/src/funciones.php
@@ -11,4 +11,40 @@
                 echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
             }
         }
-    ?>
+        function aleatorio(){
+            return rand(1,100);
+        }
+        function par($numero){
+            return $numero % 2 == 0;
+        }
+        function impar($numero){
+            return $numero%2 != 0;
+        }
+        $iteraciones=0;
+        $matriz= array();
+
+        while(true){
+            $num1 = aleatorio();
+            $num2 = aleatorio();
+            $num3 = aleatorio();
+            $iteraciones++;
+            $matriz[]= array($num1,$num2,$num3);
+            if (impar($num1) && par($num2) && impar($num3)){
+                break;
+            }
+        }
+        $filas = count($matriz);
+        $numeros = $filas * 3;
+        echo "La matriz generada es: <br>";
+        foreach ($matriz as $fila){
+            foreach($fila as $dato){
+            echo $dato. ', ';
+        }
+        echo "<br>";
+    }
+        echo "<br>";
+        echo "Número de iteraciones: $iteraciones <br>";
+        echo "Cantidad de números generados: $numeros";
+        $matriz = array();
+
+?>
