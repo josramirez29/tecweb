edd33432efc45fba8ed97dc3c0e95121501fece5
josramirez29 <144729636+josramirez29@users.noreply.github.com>
2024-09-19 21:55:33 -0600

    Ejercicio 6

--- practicas/p06/index.php
+++ practicas/p06/index.php
@@ -167,7 +167,267 @@ if (isset($_POST["edad"]) && isset($_POST["sexo"])) {
     echo "<p>Por favor, complete todos los campos del formulario.</p>";
 }
 ?>
+</fieldset>
+<h2>Ejercicio 6</h2>
+    <p>Crea en código duro un arreglo asociativo que sirva para registrar el parque vehicular de
+    una ciudad. Cada vehículo debe ser identificado por:</p>
+    <ul>
+    <li>Matrícula</li>
+    <li>Auto
+        <ul>
+        <li>Marca</li>
+        <li>Modelo (año)</li>
+        <li>Tipo (sedan | hatchback | camioneta)</li>
+        </ul>
+    </li>
+    <li>Propietario
+        <ul>
+        <li>Nombre</li>
+        <li>Ciudad</li>
+        <li>Dirección</li>
+        </ul>
+    </li>
+</ul>
+<fieldset>
+<legend>Consulta de vehículos</legend>
+<form action="http://localhost/tecweb/practicas/p06/index.php" method="post">
+    Matricula: <input type="text" name="matricula"><br>
+    <input type="submit" value="Consultar por Matrícula">
+</form>
 
+<form action="http://localhost/tecweb/practicas/p06/index.php" method="post">
+    <input type="hidden" name="todos" value="1">
+    <input type="submit" value="Consultar Todos los Vehículos">
+</form>
+<?php
+// Arreglo del parque vehicular con 15 autos
+$vehiculos = array(
+    'UBN6338' => array(
+        'Auto' => array(
+            'marca' => 'HONDA',
+            'modelo' => 2020,
+            'tipo' => 'camioneta',
+        ),
+        'Propietario' => array(
+            'nombre' => 'Alfonzo Esparza',
+            'ciudad' => 'Puebla, Pue.',
+            'direccion' => 'C.U., Jardines de San Manuel',
+        ),
+    ),
+    'UBN6339' => array(
+        'Auto' => array(
+            'marca' => 'MAZDA',
+            'modelo' => 2019,
+            'tipo' => 'sedan',
+        ),
+        'Propietario' => array(
+            'nombre' => 'Ma. del Consuelo Molina',
+            'ciudad' => 'Puebla, Pue.',
+            'direccion' => '97 oriente',
+        ),
+    ),
+    'ABC1234' => array(
+        'Auto' => array(
+            'marca' => 'NISSAN',
+            'modelo' => 2021,
+            'tipo' => 'hatchback',
+        ),
+        'Propietario' => array(
+            'nombre' => 'Rodrigo Vidal',
+            'ciudad' => 'México, CDMX',
+            'direccion' => 'Av. Insurgentes 123',
+        ),
+    ),
+    'XYZ5678' => array(
+        'Auto' => array(
+            'marca' => 'TOYOTA',
+            'modelo' => 2022,
+            'tipo' => 'camioneta',
+        ),
+        'Propietario' => array(
+            'nombre' => 'Sergio Pérez',
+            'ciudad' => 'Guadalajara, Jal.',
+            'direccion' => 'Colonia Centro',
+        ),
+    ),
+    'ABC1234' => array(
+        'Auto' => array(
+            'marca' => 'Mazda',
+            'modelo' => 2018,
+            'tipo' => 'sedan',
+        ),
+        'Propietario' => array(
+            'nombre' => 'Carlos Sainz',
+            'ciudad' => 'Monterrey, NL',
+            'direccion' => 'Calle 10',
+        ),
+    ),
+    'DEF5678' => array(
+        'Auto' => array(
+            'marca' => 'Ford',
+            'modelo' => 2019,
+            'tipo' => 'hatchback',
+        ),
+        'Propietario' => array(
+            'nombre' => 'Ivon Tepoz',
+            'ciudad' => 'Puebla, Pue.',
+            'direccion' => 'Av. 5 de Mayo',
+        ),
+    ),
+    'GHI9012' => array(
+        'Auto' => array(
+            'marca' => 'Volkswagen',
+            'modelo' => 2020,
+            'tipo' => 'camioneta',
+        ),
+        'Propietario' => array(
+            'nombre' => 'Emiliano Velez',
+            'ciudad' => 'Guadalajara, Jal.',
+            'direccion' => 'Calle Hidalgo',
+        ),
+    ),
+    'JKL3456' => array(
+        'Auto' => array(
+            'marca' => 'Seat',
+            'modelo' => 2021,
+            'tipo' => 'sedan',
+        ),
+        'Propietario' => array(
+            'nombre' => 'Valeria Arciga',
+            'ciudad' => 'Mérida, Yuc.',
+            'direccion' => 'Albert Einstein',
+        ),
+    ),
+    'MNO7890' => array(
+        'Auto' => array(
+            'marca' => 'Dodge',
+            'modelo' => 2022,
+            'tipo' => 'hatchback',
+        ),
+        'Propietario' => array(
+            'nombre' => 'Sofía Carson',
+            'ciudad' => 'Tijuana, BC',
+            'direccion' => 'Mexico 68',
+        ),
+    ),
+    'PQR1234' => array(
+        'Auto' => array(
+            'marca' => 'Mazda',
+            'modelo' => 2016,
+            'tipo' => 'sedan',
+        ),
+        'Propietario' => array(
+            'nombre' => 'Leilani Morales',
+            'ciudad' => 'Veracruz, Ver.',
+            'direccion' => 'Calle del Mar',
+        ),
+    ),
+    'STU5678' => array(
+        'Auto' => array(
+            'marca' => 'Ford',
+            'modelo' => 2017,
+            'tipo' => 'camioneta',
+        ),
+        'Propietario' => array(
+            'nombre' => 'Elena Villa',
+            'ciudad' => 'Querétaro, Qro.',
+            'direccion' => 'Calle de los Arcos',
+        ),
+    ),
+    'VWX9012' => array(
+        'Auto' => array(
+            'marca' => 'Volkswagen',
+            'modelo' => 2018,
+            'tipo' => 'hatchback',
+        ),
+        'Propietario' => array(
+            'nombre' => 'Marco Antonio Solis',
+            'ciudad' => 'Oaxaca, Oax.',
+            'direccion' => 'Calle de la Paz',
+        ),
+    ),
+    'YZA3456' => array(
+        'Auto' => array(
+            'marca' => 'Seat',
+            'modelo' => 2020,
+            'tipo' => 'sedan',
+        ),
+        'Propietario' => array(
+            'nombre' => 'Paola Durante',
+            'ciudad' => 'Pachuca, Hgo.',
+            'direccion' => 'Calle de los Héroes',
+        ),
+    ),
+    'BCD7890' => array(
+        'Auto' => array(
+            'marca' => 'Dodge',
+            'modelo' => 2021,
+            'tipo' => 'camioneta',
+        ),
+        'Propietario' => array(
+            'nombre' => 'Gala Montes',
+            'ciudad' => 'Chihuahua, Chih.',
+            'direccion' => 'Calle de la Libertad',
+        ),
+    ),
+    'EFG3456' => array(
+        'Auto' => array(
+            'marca' => 'Mazda',
+            'modelo' => 2023,
+            'tipo' => 'hatchback',
+        ),
+        'Propietario' => array(
+            'nombre' => 'Isabel Lascurain',
+            'ciudad' => 'Durango, Dgo.',
+            'direccion' => 'Calle Constitución',
+        ),
+    ),
+    'HIJ5678' => array(
+        'Auto' => array(
+            'marca' => 'Ford',
+            'modelo' => 2020,
+            'tipo' => 'sedan',
+        ),
+        'Propietario' => array(
+            'nombre' => 'José Luis Hernández',
+            'ciudad' => 'Toluca, Edomex',
+            'direccion' => 'Calle de la Reforma',
+        ),
+    ),
+);
 
+if (isset($_POST['matricula']) && $_POST['matricula'] != "") {
+    // Consulta por matrícula
+    $matricula = $_POST['matricula'];
+    if (isset($vehiculos[$matricula])) {
+        $vehiculo = $vehiculos[$matricula];
+        echo "<h3>Información del vehículo con matrícula: $matricula</h3>";
+        echo "<p>Marca: " . $vehiculo['Auto']['marca'] . "</p>";
+        echo "<p>Modelo: " . $vehiculo['Auto']['modelo'] . "</p>";
+        echo "<p>Tipo: " . $vehiculo['Auto']['tipo'] . "</p>";
+        echo "<p>Propietario: " . $vehiculo['Propietario']['nombre'] . "</p>";
+        echo "<p>Ciudad: " . $vehiculo['Propietario']['ciudad'] . "</p>";
+        echo "<p>Dirección: " . $vehiculo['Propietario']['direccion'] . "</p>";
+    } else {
+        echo "<p>No se encontró un vehículo con la matrícula $matricula.</p>";
+    }
+} elseif (isset($_POST['todos'])) {
+    // Mostrar todos los vehículos
+    echo "<h3>Lista de todos los vehículos registrados</h3>";
+    foreach ($vehiculos as $matricula => $vehiculo) {
+        echo "<h4>Matrícula: $matricula</h4>";
+        echo "<p>Marca: " . $vehiculo['Auto']['marca'] . "</p>";
+        echo "<p>Modelo: " . $vehiculo['Auto']['modelo'] . "</p>";
+        echo "<p>Tipo: " . $vehiculo['Auto']['tipo'] . "</p>";
+        echo "<p>Propietario: " . $vehiculo['Propietario']['nombre'] . "</p>";
+        echo "<p>Ciudad: " . $vehiculo['Propietario']['ciudad'] . "</p>";
+        echo "<p>Dirección: " . $vehiculo['Propietario']['direccion'] . "</p>";
+        echo "<hr>";
+    }
+} else {
+    echo "<p>Por favor, ingrese una matrícula o consulte todos los vehículos.</p>";
+}
+?>
+</fieldset>
 </body>
 </html>
