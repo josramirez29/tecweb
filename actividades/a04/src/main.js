36e650bfe5cc1a4095ac18b28e5a93d7f61193d9
josramirez29 <144729636+josramirez29@users.noreply.github.com>
2024-10-08 19:48:08 -0600

    Ejemplo 2

--- actividades/a04/src/main.js
+++ actividades/a04/src/main.js
@@ -13,3 +13,17 @@ function ejemplo1() {
     document.write('Hola Mundo');
 }
 
+function ejemplo2() {
+    var nombre = 'Jos';
+    var edad = 20;
+    var altura = 1.60;
+    var casado = false;
+    document.write( nombre );
+    document.write( '<br>' );
+    document.write( edad );
+    document.write( '<br>' );
+    document.write( altura );
+    document.write( '<br>' );
+    document.write( casado );
+}
+
