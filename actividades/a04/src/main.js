66521363b0dc9afdfb088d9ec99e5667af452310
josramirez29 <144729636+josramirez29@users.noreply.github.com>
2024-10-08 20:20:21 -0600

    Ejemplo 11

--- actividades/a04/src/main.js
+++ actividades/a04/src/main.js
@@ -156,3 +156,18 @@ function ejemplo10() {
     }
 }
 
+function ejemplo11() {
+    var x = 1;
+    var suma = 0;
+    var valor;
+
+    while (x <= 5) {
+        valor = prompt('Ingresa el valor:', '');
+        valor = parseInt(valor);
+        suma = suma + valor;
+        x = x + 1;
+    }
+
+    document.write("La suma de los valores es " + suma + "<br>");
+}
+
